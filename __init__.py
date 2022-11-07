11b6de7dfcd1d8a091fa0fc66cccad7e6de736ef
dixithlara <110405846+dixithlara@users.noreply.github.com>
2022-11-08 00:03:18 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,27 +1,43 @@
-import flask
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db=SQLAlchemy()
-DB_name="database.db"
-
+DB_name='database.db'
+print ("database.db")
 def create_app():
-    app=flask.Flask(__name__)
+    app=Flask(__name__)
     app.config['SECRET_KEY']='asdfghjkl'
-    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///{DB_name}'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_name}'
     db.init_app(app)
-
+    
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
 
+    from .models import User,Note
+    
+    create_database(app)
+    
+    login_manager=LoginManager()
+    login_manager.login_view='view.home'
+    login_manager.init_app(app)
+     
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+        
     return app
 
-def create_data(app):
-    if not path.exists('website/'+DB_name):
-       db.create_app(app=app)
-       print('created database!')
+def create_database(app):
+    if not path.exists('web1/'+DB_name):
+       with app.app_context(): 
+        db.create_all( )
+        print('created database!')
+       
+
+
+
